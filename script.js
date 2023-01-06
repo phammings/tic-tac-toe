60730210ee253a23a7178110a1de6f66d46c9e63
Ryan <ryanlepham9@gmail.com>
2023-01-05 19:28:55 -0500

    Implemented marker being added to gameboard on click

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
 const gameBoard = (() => {
 	const lines = document.querySelectorAll(".line");
+    const cells = document.querySelectorAll(".cell");
 	const hand = document.querySelector(".hand");
 	const pageTurn = document.querySelector(".page-turn");
 	const btnStartGame = document.querySelector(".btn-start-game");
+    let isCircle = true;
 
 	const addListeners = () => {
 		btnStartGame.addEventListener("click", newMatch);
+        [...cells].forEach(cell => {cell.addEventListener("click", addMarker)});
 	};
+
+    const addMarker = (event) => {
+        if (!event.target.hasAttribute("is-marked")) {
+            console.log("Marked!");
+            let img = document.createElement("img");
+            isCircle ? img.src = "resources/images/pencil-stroke-circle.png":img.src = "resources/images/pencil-stroke-short.png";
+            isCircle = !isCircle;
+            img.setAttribute("is-marked", "true");
+            event.target.appendChild(img);  
+            img.parentElement.setAttribute("is-marked", "true");
+        }
+    }
 
 	const newMatch = () => {
 		pageTurn.classList.remove("animate")
@@ -27,6 +42,8 @@ const gameBoard = (() => {
 
 	return { addListeners }
 })();
+
+// const displayController = (() => {})();
 
 
 
